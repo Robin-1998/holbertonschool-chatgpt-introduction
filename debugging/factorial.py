e9873c2bdf7525d28ffc6b7fca46777c8b2de4fc
Robin-1998 <10616@holbertonstudents.com>
2025-05-05 09:53:37 +0200

    correctif indentation

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,11 +2,11 @@
 import sys
 
 def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1  # Il faut décrémenter n pour éviter une boucle infinie
-    return result
+	result = 1
+	while n > 1:
+		result *= n
+		n -= 1 # Il faut décrémenter n pour éviter une boucle infinie
+	return result
 
 f = factorial(int(sys.argv[1]))
 print(f)
